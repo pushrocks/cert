2e8cfd00d240f99e7eb7163081dc49c4a90a22ce
PhilKunz <github@philkunz.com>
2016-07-24 19:39:20 +0200

    now paying respect to same zone subdomains

--- dist/cert.helpers.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="q" />
-import { Cert } from "./index.ts";
-import * as plugins from "./cert.plugins";
-export declare let accountsKeyPresent: () => plugins.q.Promise<{}>;
-export declare let scheduleRetry: (domainArg: string, certClassArg: Cert) => plugins.q.Promise<{}>;
